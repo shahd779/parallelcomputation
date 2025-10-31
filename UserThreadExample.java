ef4b824252b9c24790aa804e88f82c6117469a7a
shahdkamal <shahdkamal49@gmail.com>
2025-10-31 23:33:45 +0200

    Removed duplicate files from root folder after moving to task1

--- UserThreadExample.java
+++
@@ -1,9 +0,0 @@
-public class UserThreadExample {
-    public static void main(String[] args) {
-        Thread t = new Thread(() -> {
-            System.out.println("Inside user thread: " + Thread.currentThread().getName());
-        }, "UserThread-1");
-        t.start();
-        System.out.println("Main thread ends: " + Thread.currentThread().getName());
-    }
-}
